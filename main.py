1f0daa898d46f02fc55d5cb576e477dd96db1f78
Sebastian Seingier <sebastian.seingier@epita.fr>
2026-03-23 11:52:52 +0100

    Cleaned the code

--- main.py
+++ main.py
@@ -2,17 +2,13 @@ def bubble_sort(array):
     n = len(array)
     for _ in range(n):
         for i in range(n-1):
-            swap = False
             if array[i]>array[i+1]:
-                array[i],array[i+1] = array[i+1],array[i]
-                swap = True
-            # if swap == False: 
-        #     print('Ended early')
-        #     break
+                array[i],array[i+1] = array[i+1],array[i]        
+
     return array
 
 
-ml1 = [0,1,4,2,9,7,-1]
+ml1 = [0,1,4,2,9,7,-1,0,-9,0,100,-5,-6]
 print(bubble_sort(ml1))
 
 
