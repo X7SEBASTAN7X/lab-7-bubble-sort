28ef1726df477e10032ddc8640dd201bc72d6b38
Sebastian Seingier <sebastian.seingier@epita.fr>
2026-03-23 11:50:11 +0100

    Basic Bubble Sort
    Also added the photo of premium requests used and started a basic implementation based on the basic algorithm provided by the ASK? method

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+def bubble_sort(array):
+    n = len(array)
+    for _ in range(n):
+        for i in range(n-1):
+            swap = False
+            if array[i]>array[i+1]:
+                array[i],array[i+1] = array[i+1],array[i]
+                swap = True
+            # if swap == False: 
+        #     print('Ended early')
+        #     break
+    return array
 
 
-
+ml1 = [0,1,4,2,9,7,-1]
+print(bubble_sort(ml1))
 
 
 if __name__=='__main__':
